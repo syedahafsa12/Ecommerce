d266c4883a345a8582eb26eaf1a2eff0aa096056
Syeda Hafsa <syedahafsa772@gmail.com>
2024-03-24 07:07:11 +0500

    first commi

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-
+// import {visionTool} from '@sanity/vision'
 import {visionTool} from '../sanity/node_modules/@sanity/vision/lib/dts/src/index'
 import {schemaTypes} from './schemaTypes'
 
